6652014604aab35c7ec76dc34ba64465ddc755a8
JiahuiHuang <1547518881@qq.com>
2021-02-08 22:15:37 +0000

    practival 2

--- src/com/praticals/p2/q2.java
+++ src/com/praticals/p2/q2.java
@@ -1,4 +1,18 @@
 package com.praticals.p2;
+import java.util.Scanner;
 
 public class q2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the length from the center of a pentagon to a vertex:");
+
+        double usrLen = scan.nextFloat();
+        if (usrLen <= 0 || usrLen > 1000){
+            System.out.println("Length must be in range 0-1000.");
+            System.exit(0);
+        }
+
+        double vertexArea = 5 * Math.pow(usrLen, 2)/(4 * Math.tan(Math.PI/5));
+        System.out.println("Area of the vertex is "+vertexArea);
+    }
 }
